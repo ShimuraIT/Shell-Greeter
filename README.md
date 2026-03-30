ee40a930d23c4acd4a95a3efb904440ec0045a21
Lucky <254033498+ShimuraIT@users.noreply.github.com>
2026-03-30 09:29:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Good afternoon, user.
+is just about all it should say, ever.
+if you want to rng out a motivational quotation, go ahead.
+if you want emojis, go nuts.
+
+### Highly customizable
+because it's 40 lines of shell script.
+it greets you with your first name, because GECOS has no spot for an honorific; e.g. "Good afternoon, Mr. Shimura."
+it would be a great deal more complex, but you could ask and store that info.
+
+### Installation
+run it with your shell rc script i.e. `~/.bashrc`
---
+++ greet.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+FIRST_NAME=$(getent passwd "$USER" | cut -d: -f5 | cut -d, -f1 | cut -d' ' -f1)
+#1. (getent) Start with user info,
+#2,3. (cut, cut) strip it to the name,
+#4. (cut) Pull only the first.
+
+#Test if empty, fall back on username.
+if [ -z "$FULL_NAME" ]; then
+	FIRST_NAME="$USER"
+fi
+
+#Get the hour
+HOUR=$(date +"%H")
+
+#Default to evening, for very early morning work.
+TIME_GREETING="evening"
+
+#A question I love to ask people is "Is 5 o'clock the afternoon, or evening?"
+#I'm opting for 5 o'clock to be evening, because it's a nice confirmation that
+#it's almost time to clock out.
+
+#If it's before 5:00 PM
+if [ $HOUR -lt 17 ]; then
+	#If it's 4:00 AM or later
+	if [ $HOUR -gt 3 ]; then
+		TIME_GREETING="morning"
+	fi
+	#If it's 12:00 PM or later
+	if [ $HOUR -gt 11 ]; then
+		TIME_GREETING="afternoon"
+	fi
+fi
+
+#Greet user based on time
+echo "Good $TIME_GREETING, $FIRST_NAME."
